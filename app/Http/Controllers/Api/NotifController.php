24e04af444f06b8c8b10b2078d6848e8ea286de0
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-07-27 22:39:22 +0700

    notif

---
+++ app/Http/Controllers/Api/NotifController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+Use App\Notif;
+
+class NotifController extends Controller
+{
+    public function index(){
+        $notif = Notif::all()->get();
+        return response()->json([
+           'message' => 'berhasil',
+           'status' => 1,
+           'data' => $notif
+        ], 201);
+
+    }
+    public function store(Request $request){
+        $notif = Notif::create([
+            'isi_notif' => $request->isi_notif
+        ]);
+
+        return response()->json([
+            'message' => 'berhasil',
+            'status' => 1,
+            'data' => $notif
+        ], 201);
+
+    }
+}
