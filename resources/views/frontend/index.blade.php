3abced91b5f4d0b0785316c60af803002bf0af7a
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-08-04 06:54:01 +0700

    frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -206,48 +206,37 @@
             <div class="col-lg-4 col-md-7 col-sm-9">
                 <div class="single-features mt-40">
                     <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Graphics Design</a></h4>
+                        <h4 class="features-title"><a href="#">DEBIT TUMPAH</a></h4>
                         <div class="features-icon">
                             <i class="lni lni-brush"></i>
-                            <img class="shape" src="{{asset('frontend/assets/images/f-shape-1.svg')}}" alt="Shape">
+                            <h4 alt="Shape" id="debit"></h4>
+                            {{--<img class="shape" src="{{asset('frontend/assets/images/f-shape-1.svg')}}" alt="Shape">--}}
                         </div>
                     </div>
                     <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
+                        {{--<p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>--}}
+                        <a class="features-btn" href="#" id="statusd"></a>
                     </div>
                 </div> <!-- single features -->
             </div>
             <div class="col-lg-4 col-md-7 col-sm-9">
                 <div class="single-features mt-40">
                     <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Website Design</a></h4>
+                        <h4 class="features-title"><a href="#">SUNGAI</a></h4>
                         <div class="features-icon">
                             <i class="lni lni-layout"></i>
-                            <img class="shape" src="{{asset('frontend/assets/images/f-shape-1.svg')}}" alt="Shape">
+                            <h4 alt="Shape" id="sungai"></h4>
+
+                            {{--<img class="shape" src="{{asset('frontend/assets/images/f-shape-1.svg')}}" alt="Shape">--}}
                         </div>
                     </div>
                     <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
+                        {{--<p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>--}}
+                        <a class="features-btn" href="#" id="statuss"></a>
                     </div>
                 </div> <!-- single features -->
             </div>
-            <div class="col-lg-4 col-md-7 col-sm-9">
-                <div class="single-features mt-40">
-                    <div class="features-title-icon d-flex justify-content-between">
-                        <h4 class="features-title"><a href="#">Digital Marketing</a></h4>
-                        <div class="features-icon">
-                            <i class="lni lni-bolt"></i>
-                            <img class="shape" src="{{asset('frontend/assets/images/f-shape-1.svg')}}" alt="Shape">
-                        </div>
-                    </div>
-                    <div class="features-content">
-                        <p class="text">Short description for the ones who look for something new. Short description for the ones who look for something new.</p>
-                        <a class="features-btn" href="#">LEARN MORE</a>
-                    </div>
-                </div> <!-- single features -->
-            </div>
+
         </div> <!-- row -->
     </div> <!-- container -->
 </section>
@@ -394,20 +383,20 @@
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <div class="footer-logo text-center">
-                    <a class="mt-30" href="index.html"><img src="{{ asset('frontend/assets/images/logo.svg')}}" alt="Logo"></a>
+                    <a class="mt-30" href="{{route('/')}}"><img src="{{ asset('frontend/assets/images/logo.svg')}}" alt="Logo"></a>
                 </div> <!-- footer logo -->
                 <ul class="social text-center mt-60">
-                    <li><a href="https://facebook.com/uideckHQ"><i class="lni lni-facebook-filled"></i></a></li>
-                    <li><a href="https://twitter.com/uideckHQ"><i class="lni lni-twitter-original"></i></a></li>
-                    <li><a href="https://instagram.com/uideckHQ"><i class="lni lni-instagram-original"></i></a></li>
+                    {{--<li><a href="https://facebook.com/uideckHQ"><i class="lni lni-facebook-filled"></i></a></li>--}}
+                    <li><a href="https://twitter.com/IBrebes_Flood"><i class="lni lni-twitter-original"></i></a></li>
+                    {{--<li><a href="https://instagram.com/uideckHQ"><i class="lni lni-instagram-original"></i></a></li>--}}
                     <li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>
                 </ul> <!-- social -->
-                <div class="footer-support text-center">
-                    <span class="number">+8801234567890</span>
-                    <span class="mail">support@uideck.com</span>
-                </div>
+                {{--<div class="footer-support text-center">--}}
+                    {{--<span class="number">+8801234567890</span>--}}
+                    {{--<span class="mail">support@uideck.com</span>--}}
+                {{--</div>--}}
                 <div class="copyright text-center mt-35">
-                    <p class="text">Designed by <a href="https://uideck.com" rel="nofollow">UIdeck</a> and Built-with <a rel="nofollow" href="https://ayroui.com">Ayro UI</a> </p>
+                    <p class="text">Hanya untuk simulasi tugas akhir</p>
                 </div> <!--  copyright -->
             </div>
         </div> <!-- row -->
