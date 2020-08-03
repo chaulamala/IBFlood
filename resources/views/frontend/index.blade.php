0dd19a4852e646f603eb8a6217476fbff614fc6a
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-08-03 22:00:26 +0700

    frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -34,9 +34,9 @@
 </head>
 
 <body>
-<!--[if IE]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-<![endif]-->
+
+<!--<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>-->
+
 
 <!--====== PRELOADER PART START ======-->
 
