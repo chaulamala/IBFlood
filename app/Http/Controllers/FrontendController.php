3a94c96ae10daff758d54e91ef66d60a7d231b0a
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-08-04 07:43:11 +0700

    ubah

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Masyarakat;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function daftar(Request $request){
+
+    }
+}
---
+++ database/migrations/2020_08_04_073647_create_masyarakats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMasyarakatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masyarakats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('no_hp','13');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('masyarakats');
+    }
+}
