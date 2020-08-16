aa52d8c8b039c25aae4c1b49244b57ed8519ef81
chaulamala <chaulanoormala@gmail.com>
2020-08-16 15:21:13 +0700

    ubah

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -63,7 +63,7 @@ class ReportController extends Controller
 
     public function sungai()
     {
-        $sungai = Report::select('created_at', 'sungai')->orderBy('id', 'ASC')->get();
+        $sungai = Report::select('created_at', 'sungai')->orderBy('id', 'asc')->get();
         return view('pages.ketinggiansungai', compact('sungai'));
     }
 
